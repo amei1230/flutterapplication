77f73ca2a50df8b454ea3686ac5c971ea7c3a770
amei1230 <amenichouchene21@gmail.com>
2025-10-23 12:25:54 +0100

    atelier1

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'atelier1.dart'; // ou 'atelier2.dart' pour tester l'autre atelier
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Material 3',
+      theme: ThemeData(useMaterial3: true),
+      home: const ProfilePageM3(), // ou ProductListPageM3()
+    );
+  }
+}
