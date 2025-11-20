4d8cef049aed27476099311d316b2e9527276b20
Ameni chouchen <amenichouchene21@gmail.com>
2025-11-20 16:12:06 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'atelier1.dart'; // ou 'atelier2.dart' pour tester l'autre atelier
-import 'atelier2.dart';
+// ou 'atelier2.dart' pour tester l'autre atelier
+import 'atelier6.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,14 +8,13 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Material 3',
       theme: ThemeData(useMaterial3: true),
       //home: const ProfilePageM3(), // ou ProductListPageM3()//
-      home: const ProductListPageM3(),
+      home: const Atelier6Page(),
     );
   }
 }
